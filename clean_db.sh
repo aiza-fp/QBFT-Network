34ab083a5cebdc4869595c13a5bfbfcde85d4f30
aiza-fp <94653085+aiza-fp@users.noreply.github.com>
2023-12-12 16:31:44 +0100

    remove containers first

--- clean_db.sh
+++ clean_db.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
+docker container stop $(docker container ls -aq)
+docker container prune -f
 rm -r ./Node/data/caches
 rm -r ./Node/data/database
 rm ./Node/data/DATABASE_METADATA.json
-
